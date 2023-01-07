5673d57579426cdb00bc03d6464ccfcead9b0d90
shandy siswandi <shandy.siswandi@virgoku.id>
2023-01-07 21:11:32 +0700

    how to print something in rust

---
+++ src/print.rs
@@ -0,0 +1,11 @@
+pub fn run() {
+    println!("===== Print to Console =====");
+
+    println!("this is the way to print with new line");
+
+    print!("this is the way to print without new line \n");
+
+    println!("this is how to print with {}", "placeholder");
+
+    println!();
+}
