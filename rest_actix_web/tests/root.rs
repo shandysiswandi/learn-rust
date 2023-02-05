f21c13e8d6dd97648bb1bb597843884bc64d1cd5
SHANDY SISWANDI <shandysiswandi@gmail.com>
2023-02-05 15:36:34 +0700

    add integration test fn no_route

--- rest_actix_web/tests/root.rs
+++ rest_actix_web/tests/root.rs
@@ -4,7 +4,7 @@ use common::*;
 use reqwest::{Client, Response};
 
 #[actix_rt::test]
-async fn root_index_ok() {
+async fn root_index() {
     // Arrange
     let app: TestApp = spawn_app().await;
     let client: Client = Client::new();
@@ -25,7 +25,7 @@ async fn root_index_ok() {
 }
 
 #[actix_rt::test]
-async fn root_health_ok() {
+async fn root_health() {
     // Arrange
     let app: TestApp = spawn_app().await;
     let client: Client = Client::new();
@@ -41,4 +41,25 @@ async fn root_health_ok() {
 
     // Assert
     assert_eq!(code, 200);
+}
+
+#[actix_rt::test]
+async fn root_no_route() {
+    // Arrange
+    let app: TestApp = spawn_app().await;
+    let client: Client = Client::new();
+
+    // Act
+    let resp: Response = client
+        .get(&format!("{}/not-found", &app.address))
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    let code: u16 = resp.status().as_u16();
+    let body: String = resp.text().await.expect("Failed to decode body");
+
+    // assert
+    assert_eq!(code, 404);
+    assert_eq!(body, "{\"message\":\"route not found\"}");
 }
