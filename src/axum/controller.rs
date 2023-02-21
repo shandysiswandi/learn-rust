9a9378a4dafdf41514ca70700ea45a96f46246cb
SHANDY SISWANDI <shandysiswandi@gmail.com>
2023-02-21 21:34:05 +0700

    refactor axum

--- src/axum/controller.rs
+++ src/axum/controller.rs
@@ -17,29 +17,17 @@ use std::collections::HashMap;
 use tokio::time::{sleep, Duration};
 use validator::Validate;
 
-pub async fn path_variable(Path(name): Path<String>) -> impl IntoResponse {
-  tracing::info!("listening on {}", name);
-  format!("Hello {}", name)
-}
+//////////////////////////////////////////////////////////////////////
 
-pub async fn status_code() -> impl IntoResponse {
-  StatusCode::OK
-}
-
-pub async fn html() -> impl IntoResponse {
+pub async fn ct_html() -> impl IntoResponse {
   Html(r#"<h1>Hello from html</h1>"#)
 }
 
-pub async fn json() -> impl IntoResponse {
+pub async fn ct_json() -> impl IntoResponse {
   Json(vec!["satu", "dua", "tiga"])
 }
 
-pub async fn graceful_shutdown() -> impl IntoResponse {
-  sleep(Duration::from_secs(5)).await;
-  "server graceful shutdown"
-}
-
-pub async fn validation(Json(input): Json<NameInput>) -> impl IntoResponse {
+pub async fn ct_validation(Json(input): Json<NameInput>) -> impl IntoResponse {
   let Ok(_) = input.validate() else {
     return (StatusCode::BAD_REQUEST, "invalid argument".to_string());
   };
@@ -47,8 +35,44 @@ pub async fn validation(Json(input): Json<NameInput>) -> impl IntoResponse {
   (StatusCode::OK, format!("your input name: {}", input.name))
 }
 
-pub async fn form(Form(input): Form<NameInput>) -> impl IntoResponse {
+pub async fn ct_form(Form(input): Form<NameInput>) -> impl IntoResponse {
   (StatusCode::OK, format!("your input name: {}", input.name))
+}
+
+//////////////////////////////////////////////////////////////////////
+
+pub async fn crud_list() -> impl IntoResponse {
+  format!("Hello crud list")
+}
+
+pub async fn crud_read(Path(id): Path<usize>) -> impl IntoResponse {
+  format!("Hello read todo({})", id)
+}
+
+pub async fn crud_create(Json(input): Json<NameInput>) -> impl IntoResponse {
+  format!("Hello create todo({})", input.name)
+}
+
+pub async fn crud_update(Path(id): Path<usize>, Json(input): Json<NameInput>) -> impl IntoResponse {
+  format!("Hello update todo({},{})", id, input.name)
+}
+
+pub async fn crud_partial_update(
+  Path(id): Path<usize>,
+  Json(input): Json<NameInput>,
+) -> impl IntoResponse {
+  format!("Hello update todo({},{})", id, input.name)
+}
+
+pub async fn crud_delete(Path(id): Path<usize>) -> impl IntoResponse {
+  format!("Hello delete todo({})", id)
+}
+
+//////////////////////////////////////////////////////////////////////
+
+pub async fn graceful_shutdown() -> impl IntoResponse {
+  sleep(Duration::from_secs(5)).await;
+  "server graceful shutdown"
 }
 
 pub async fn with_rejection(
@@ -57,6 +81,8 @@ pub async fn with_rejection(
 ) -> impl IntoResponse {
   Json(vec!["satu", "dua", "tiga"])
 }
+
+//////////////////////////////////////////////////////////////////////
 
 pub async fn fallback() -> impl IntoResponse {
   Json(HashMap::from([(
--- src/axum/mod.rs
+++ src/axum/mod.rs
@@ -1,25 +1,39 @@
 mod controller;
 mod error;
 mod middleware;
-pub mod model;
+mod model;
 
+use self::controller as h;
 use axum::{routing, Router};
-use controller as h;
 use tower_http::trace::TraceLayer;
 
 pub fn init() -> Router {
   let root = Router::new()
-    .route("/:name", routing::get(h::path_variable))
-    .route("/status-code", routing::get(h::status_code))
-    .route("/html", routing::get(h::html))
-    .route("/json", routing::get(h::json))
+    // reject is custom error when request Extractor for customizing extractor rejections
     .route("/reject", routing::post(h::with_rejection))
-    .route("/graceful-shutdown", routing::get(h::graceful_shutdown))
-    .route("/form", routing::post(h::form))
-    .route("/validation", routing::post(h::validation));
+    // graceful-shutdown is route to delay 5 second, to test gracefull shutdown is ok
+    .route("/graceful-shutdown", routing::get(h::graceful_shutdown));
+
+  // list of endpoint that handle request or response with different content type
+  let content_types = Router::new()
+    .route("/validation", routing::post(h::ct_validation))
+    .route("/form", routing::post(h::ct_form))
+    .route("/html", routing::get(h::ct_html))
+    .route("/json", routing::get(h::ct_json));
+
+  // list of endpoint crud
+  let todos = Router::new()
+    .route("/", routing::get(h::crud_list))
+    .route("/:id", routing::get(h::crud_read))
+    .route("/", routing::post(h::crud_create))
+    .route("/:id", routing::put(h::crud_update))
+    .route("/:id", routing::patch(h::crud_partial_update))
+    .route("/:id", routing::delete(h::crud_delete));
 
   Router::new()
     .nest("/", root)
+    .nest("/crud", todos)
+    .nest("/content-types", content_types)
     .layer(middleware::cors())
     .layer(TraceLayer::new_for_http())
     .fallback(h::fallback)
